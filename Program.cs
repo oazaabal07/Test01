91187fd717f6f57c8a693d25d2ece06e28ea4378
hp <hp@ABD-ELMOUNIM>
2023-10-18 15:45:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is the number that will start with
+            string num = "1";
+
+            //I defined here number 10 to give me 10 lines
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(num);
+                num = testFunction(num);
+            }
+        }
+
+        public static string testFunction(String number)
+        {
+            StringBuilder result = new StringBuilder();
+
+            char repeat = number[0];
+            number = number.Substring(1) + " ";
+            int time = 1;
+
+            foreach (char actual in number)
+            {
+                if (actual != repeat)
+                {
+                    result.Append(time.ToString() + repeat);
+                    time = 1;
+                    repeat = actual;
+                }
+                else
+                    time++;
+            }
+
+            return result.ToString();
+        }
+    }
+}
